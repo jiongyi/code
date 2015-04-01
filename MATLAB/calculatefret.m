88e3ab04f584289460e29200ec89f8e9c8e519e7
Jiongyi Tan <jiongyi@users.noreply.github.com>
2015-03-31 21:41:13 -0700

    Create calculatefret.m

---
+++ MATLAB/calculatefret.m
@@ -0,0 +1,10 @@
+function effIm = calculatefret(donorIm, fretIm, acceptorIm)
+    % Correct for bleed-through and cross-talk.
+    corrIm = imsubtract(fretIm, 0.47 * donorIm);
+    corrIm = imstruct(corrIm, 0.18 * acceptorIm);
+    % Calculate FRET efficiency.
+    effIm = corrIm ./ acceptorIm;
+    % Zero-out background.
+    bwIm = im2bw(acceptorIm, graythresh(mat2gray(acceptorIm)));
+    % effIm(~bwIm) = 0;
+end
