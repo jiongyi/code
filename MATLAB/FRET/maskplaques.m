387390c2634e29c7f77987509a11f440b41485e7
Jiongyi Tan <joogle.t@gmail.com>
2015-04-20 17:52:41 -0700

    Worked on plaque detection code

--- MATLAB/FRET/maskplaques.m
+++ MATLAB/FRET/maskplaques.m
@@ -1,13 +1,27 @@
-function maskplaques(im)
+function maskplaques(rawIm, objPixWidth)
 
-im = im2double(im);
-im = mat2gray(im);
+% Convert and double and scale to [0, 1].
+rawIm = im2double(rawIm);
+rawIm = mat2gray(rawIm);
+rawIm = medfilt2(rawIm);
 
-psfFilteredIm = imfilter(im, fspecial('gaussian', 3, 1), 'symmetric');
-backFilteredIm = imfilter(im, fspecial('gaussian', 300, 100), 'symmetric');
-im = mat2gray(psfFilteredIm - backFilteredIm);
-thresholdRow = multithresh(im, 10);
-labeledIm = imquantize(im, thresholdRow);
-rgbIm = label2rgb(labeledIm);
-figure;
-imshowpair(im, rgbIm, 'montage');
+% Apply difference-of-gaussian filter.
+psfFilteredIm = imfilter(rawIm, fspecial('gaussian', 3, 1), 'symmetric');
+backFilteredIm = imfilter(rawIm, ...
+    fspecial('average', objPixWidth), 'symmetric');
+dogIm = imsubtract(psfFilteredIm, backFilteredIm);
+dogIm(rawIm < 0) = 0;
+dogIm = mat2gray(dogIm);
+
+% Convert to signal-to-noise ratio representation.
+bwIm = im2bw(dogIm, graythresh(dogIm));
+snrIm = dogIm / mean(dogIm(~bwIm));
+
+snrIm = snrIm - mean(snrIm(bwIm));
+snrIm(snrIm < 0) = 0;
+figure('color', 'w');
+imshow(snrIm, []);
+colorHandle = colorbar;
+colorTitleHandle = get(colorHandle, 'Title');
+set(colorTitleHandle, 'String', 'SNR');
+axis square off;
