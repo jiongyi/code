4a4a6c68fd15cd07fcc2e8ab58b9ac9674db8053
Jiongyi Tan <joogle.t@gmail.com>
2015-06-21 14:25:56 -0700

    Added saving functionality

--- MATLAB/FRAP/fitfrap.m
+++ MATLAB/FRAP/fitfrap.m
@@ -1,26 +1,25 @@
 function [frapFit, frapGof] = fitfrap(timeInterval)
-% Select and load files.
+%% Select and load files.
 [preBleachFileName, folderName] = uigetfile('FRAP Pre Series*.tif', ...
     'Select pre-bleach image sequence');
 preBleachImCell = imreadtiff([folderName, preBleachFileName]);
-
 postBleachFileName = uigetfile('FRAP Pb1 Series*.tif', ...
     'Select post-bleach image sequence', folderName);
 postBleachImCell = imreadtiff([folderName, postBleachFileName]);
 
-% Average pre- and post-bleach frames.
+%% Average pre- and post-bleach frames.
 preBleachMeanIm = mean(cat(3, preBleachImCell{:}), 3);
 postBleachMeanIm = mean(cat(3, postBleachImCell{1:3}), 3);
 
-% Figure out bleached region.
+%% Figure out bleached region.
 diffIm = preBleachMeanIm - postBleachMeanIm;
-diffIm(diffIm < 0) = 0;
 diffIm = mat2gray(diffIm);
 isBleachedIm = im2bw(diffIm, graythresh(diffIm));
 isBleachedIm = imopen(isBleachedIm, strel('square', 3));
 isBleachedIm = imclearborder(isBleachedIm);
 isBleachedIm = imfill(isBleachedIm, 'holes');
 isBleachedIm = bwareaopen(isBleachedIm, 25);
+% Check is bleached region mask has only one region.
 regPropStruct = regionprops(isBleachedIm, 'Area', 'PixelIdxList');
 if numel(regPropStruct) == 0
     error('No bleach point detected. Closing.');
@@ -31,46 +30,48 @@ elseif numel(regPropStruct) > 1
         max([regPropStruct(:).Area])).PixelIdxList) = true;
 end
 
-% Compute mean post-bleach intensities.
-postBleachMeanROIRow = cellfun(@(x) mean(x(isBleachedIm)), ...
-    postBleachImCell);
-
-% Normalize by pre-bleach mean intensity.
-postBleachMeanROIRow = postBleachMeanROIRow / ...
-    mean(preBleachMeanIm(isBleachedIm));
-
-% Fit bleaching curve.
+%% Calculate bleaching correction.
 postBleachMeanIm = mat2gray(postBleachMeanIm);
 isSignalIm = im2bw(postBleachMeanIm, graythresh(postBleachMeanIm));
 postBleachMeanSignalRow = cellfun(@(x) mean(x(isSignalIm)), ...
     postBleachImCell);
-
 timeRow = cumsum([0, timeInterval * ones(1, numel(postBleachImCell) - 1)]);
 bleachFit = fit(timeRow', postBleachMeanSignalRow', 'poly1');
-% figure;
-% plot(timeRow, postBleachMeanSignalRow, 'bx');
-% hold on;
-% plot(timeRow, bleachFit(timeRow), 'r');
-% hold off;
 
+%% Compute mean post-bleach intensities.
+postBleachMeanROIRow = cellfun(@(x) mean(x(isBleachedIm)), ...
+    postBleachImCell);
+% Normalize by pre-bleach mean intensity.
+postBleachMeanROIRow = postBleachMeanROIRow / ...
+    mean(preBleachMeanIm(isBleachedIm));
 % Correct for photobleaching.
 postBleachMeanROIRow = postBleachMeanROIRow - bleachFit.p1 * timeRow;
-% figure;
-% plot(timeRow, postBleachMeanROIRow, 'bx');
-
-% Offset.
+% Offset by first postbleach measurement.
 postBleachMeanROIRow = postBleachMeanROIRow - postBleachMeanROIRow(1);
 
-% Fit.
+%% Fit.
 [frapFit, frapGof] = fit(timeRow', postBleachMeanROIRow', ...
     'm * (1 - exp(-x / k))', 'Start', [50, 0.5]);
+% Check residuals.
+residualsRow = frapFit(timeRow)' - postBleachMeanROIRow;
+[h, p] = adtest(residualsRow);
+hResiAutoCorr = figure('color', 'w');
+autocorr(residualsRow);
+box off;
+set(gca, 'tickdir', 'out');
+if h == true
+    disp(['The residuals are not distributed normally (', ...
+        num2str(p), ')'])
+end
 
-% Display results.
-figure('color', 'w');
-plot(timeRow, postBleachMeanROIRow, 'bx');
+%% Display results.
+hFRAPFig = figure('color', 'w');
+plot(frapFit, timeRow, postBleachMeanROIRow);
+ciFRAPRow = predint(frapFit, timeRow);
 hold on;
-plot(timeRow, frapFit(timeRow), 'r');
+plot(timeRow, ciFRAPRow, 'r--');
 hold off;
+legend off;
 box off;
 set(gca, 'tickdir', 'out');
 xlabel('Time (s)');
@@ -81,8 +82,13 @@ print(gcf, '-dpdf', [folderName, postBleachFileName(1 : end - 4), ...
 isPerimIm = bwperim(isBleachedIm);
 overlaidImCell = cellfun(@(x) imoverlay(x, isPerimIm, [0, 1, 0]), ...
     postBleachImCell, 'UniformOutput', false);
-
 implay(cat(4, overlaidImCell{:}), 4);
+
+%% Save results.
+print(hFRAPFig, '-dpdf', [folderName, postBleachFileName(1 : end - 4), ...
+    '_frap_curve']);
+print(hResiAutoCorr, '-dpdf', [folderName, ...
+    postBleachFileName(1 : end - 4), '_resi_autocorr']); 
 end
 
 function tiffCell = imreadtiff(tiffFilePath)
