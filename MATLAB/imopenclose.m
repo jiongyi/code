1dcdb6d83ed976d7313af162ecc8987fd395d16e
Jiongyi Tan <joogle.t@gmail.com>
2015-07-24 16:19:23 -0700

    Slight change in open-close strel

--- MATLAB/imopenclose.m
+++ MATLAB/imopenclose.m
@@ -1,7 +1,7 @@
 function openClosedIm = imopenclose(rawIm, objWidth)
 % Open-close median-filtered image.
-erodedIm = imerode(rawIm, strel('disk', objWidth));
+erodedIm = imerode(rawIm, strel('square', objWidth));
 openedIm = imreconstruct(erodedIm, rawIm);
-dilatedIm = imdilate(openedIm, strel('disk', objWidth));
+dilatedIm = imdilate(openedIm, strel('square', objWidth));
 openClosedIm = imcomplement(imreconstruct(imcomplement(dilatedIm), ...
     imcomplement(openedIm)));
--- MATLAB/martijn/maskcells.m
+++ MATLAB/martijn/maskcells.m
@@ -15,19 +15,23 @@ Nucleus.eqIm = adapthisteq(Nucleus.normIm);
 Contact.dogIm = dogfilter(Contact.eqIm, contactWidth);
 Nucleus.dogIm = dogfilter(Nucleus.eqIm, nucleusWidth);
 
+% Open-close images.
+Contact.ocIm = mat2gray(imopenclose(Contact.dogIm, contactWidth));
+Nucleus.ocIm = mat2gray(imopenclose(Nucleus.dogIm, nucleusWidth));
+
 % Segment nuclei.
-Nucleus.bwIm = im2bw(Nucleus.dogIm, graythresh(Nucleus.dogIm));
+Nucleus.bwIm = im2bw(Nucleus.ocIm, graythresh(Nucleus.ocIm));
 
 % Impose minima.
-Contact.minIm = imimposemin(Contact.dogIm, ...
+Contact.minIm = imimposemin(Contact.ocIm, ...
     imerode(Nucleus.bwIm, strel('square', 3)));
 
 % Watershed.
 Contact.waterIm = watershed(Contact.minIm);
 Contact.bwIm = imclearborder(Contact.waterIm > 1);
 
-% Display.
-figure;
-imshowpair(Contact.minIm, ...
-    label2rgb(Contact.waterIm, 'jet', 'w', 'shuffle'), 'montage');
+% Make fused image.
+Contact.outIm = cat(3, zeros(size(Contact.bwIm)), bwperim(Contact.bwIm), ...
+    zeros(size(Contact.bwIm)));
+Contact.fusedIm = imfuse(Contact.ocIm, Contact.outIm, 'blend');
 end
