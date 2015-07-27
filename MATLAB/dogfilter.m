87e283f7609a266ac893f533ee66416bbc1161be
Jiongyi Tan <joogle.t@gmail.com>
2015-07-27 10:59:35 -0700

    Cleaned up cell orientation code

--- MATLAB/dogfilter.m
+++ MATLAB/dogfilter.m
@@ -1,8 +1,12 @@
 function dogIm = dogfilter(rawIm, objWidth)
+% Applies difference-of-gaussian filtering to rawIm. Kernel size is
+% determined from objWidth. The output is scaled to [0, 1].
 doubleIm = im2double(rawIm);
+
 % Calculate sigmas.
 sigma1 = objWidth / (1 + sqrt(2));
 sigma2 = sqrt(2) * objWidth;
+
 % Filter and subtract.
 smallIm = imfilter(doubleIm, fspecial('gaussian', ...
     ceil(6 * sigma1 + 1), sigma1), 'replicate');
--- MATLAB/imopenclose.m
+++ MATLAB/imopenclose.m
@@ -1,4 +1,9 @@
 function openClosedIm = imopenclose(rawIm, objWidth)
+% Open-closes rawIm using a square structuring element of width objWidth.
+% Convert to double if rawIm is still uint*.
+if strcmp(class(rawIm), 'double')
+    rawIm = im2double(rawIm);
+end
 % Open-close median-filtered image.
 erodedIm = imerode(rawIm, strel('square', objWidth));
 openedIm = imreconstruct(erodedIm, rawIm);
