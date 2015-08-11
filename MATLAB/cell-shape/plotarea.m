0fb781cbdc8ae3fb598659588f4e598866a2056e
Jiongyi Tan <joogle.t@gmail.com>
2015-08-11 13:11:34 -0700

    Updated area plotter to display normalized areas

--- MATLAB/cell-shape/plotarea.m
+++ MATLAB/cell-shape/plotarea.m
@@ -8,8 +8,11 @@ for iFrame = 1 : noFrames
     semAreaRow(iFrame) = std([CellStatsCell{iFrame}(:).Area]) / ...
         sqrt(noCells);
 end
+normMeanAreaRow = meanAreaRow / meanAreaRow(1);
+normStdAreaRow = stdAreaRow / stdAreRow(1);
+prctMeanAreaRow = (normMeanAreaRow - 1) * 100;
 figure('color', 'w');
-errorbar(meanAreaRow, semAreaRow);
+errorbar(prctMeanAreaRow, normStdAreaRow);
 set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
 xlabel('Frame No');
 ylabel('Mean cell area');
