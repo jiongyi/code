f11226996df73846245c0b95e1cc3375815175f1
Jiongyi Tan <joogle.t@gmail.com>
2015-08-11 13:13:43 -0700

    Fixed typo in orientation plotter

--- MATLAB/cell-shape/plotorientation.m
+++ MATLAB/cell-shape/plotorientation.m
@@ -2,7 +2,7 @@ function plotorientation(CellStatsCell)
 orientationRadCell = cellfun(@(x) [x(:).Orientation], CellStatsCell, ...
     'UniformOutput', false);
 figure('color', 'w');
-plotStread(orientationRadCell);
+plotSpread(orientationRadCell);
 set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
 xlabel('Frame No');
 ylabel('Percent horizontal strain');
