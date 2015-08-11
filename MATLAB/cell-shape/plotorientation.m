83c5f029d776385606fa6bb27c531d903058e2f5
Jiongyi Tan <joogle.t@gmail.com>
2015-08-11 13:18:27 -0700

    Updated orientation plotter

--- MATLAB/cell-shape/plotorientation.m
+++ MATLAB/cell-shape/plotorientation.m
@@ -1,9 +1,18 @@
 function plotorientation(CellStatsCell)
 orientationRadCell = cellfun(@(x) [x(:).Orientation], CellStatsCell, ...
     'UniformOutput', false);
+cosOrientationCell = cellfun(@cos, orientationCell, ...
+    'UniformOutput', false);
+meanCosOrientationRow = cellfun(@mean, cosOrientationCell);
 figure('color', 'w');
 plotSpread(orientationRadCell);
 set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
 xlabel('Frame No');
-ylabel('Percent horizontal strain');
+ylabel('Orientation (rad)');
+
+figure('color', 'w');
+plot(meanCosOrientationRow);
+set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
+xlabel('Frame No');
+ylabel('Mean cosine of orientation');
 end
