2cf5c0c8bc6d7f7eabc0769f945c6c995c53a4b1
Jiongyi Tan <joogle.t@gmail.com>
2015-08-11 14:20:45 -0700

    Switched to stacked bar graph for orientation measurements

--- MATLAB/cell-shape/plotorientation.m
+++ MATLAB/cell-shape/plotorientation.m
@@ -1,31 +1,16 @@
 function plotorientation(CellStatsCell)
-orientationDegCell = cellfun(@(x) [x(:).Orientation], CellStatsCell, ...
-    'UniformOutput', false);
-cosOrientationCell = cellfun(@cos, orientationDegCell, ...
-    'UniformOutput', false);
-meanCosOrientationRow = cellfun(@mean, cosOrientationCell);
-
-orientationAbsDegCell = cellfun(@abs, orientationDegCell, ...
-    'UniformOutput', false);
-binRow = linspace(0, 90, 6);
-binCountCell = cellfun(@(x) hist(x, binRow), orientationAbsDegCell, ...
+orientationAbsDegCell = cellfun(@(x) abs([x(:).Orientation]), ...
+    CellStatsCell, 'UniformOutput', false);
+binRow = linspace(0, 90, 7);
+binCountCell = cellfun(@(x) histc(x, binRow), orientationAbsDegCell, ...
     'UniformOutput', false);
 normBinCountCell = cellfun(@(x) x / sum(x), binCountCell, ...
     'UniformOutput', false);
-normBinCountMat = vertcat(normBinCountCell{:})';
+normBinCountMat = vertcat(normBinCountCell{:});
 figure('color', 'w');
-imshow(normBinCountMat, 'InitialMagnification', 'fit');
+bar(normBinCountMat, 'stacked');
+set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
+xlabel('Frame No');
+ylabel('Frequency');
 colormap jet;
-
-figure('color', 'w');
-plotSpread(orientationDegCell);
-set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
-xlabel('Frame No');
-ylabel('Orientation (rad)');
-
-figure('color', 'w');
-plot(meanCosOrientationRow);
-set(gca, 'box', 'off', 'tickdir', 'out', 'linewidth', 1.5);
-xlabel('Frame No');
-ylabel('Mean cosine of orientation');
 end
