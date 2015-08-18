a332865fcf923f632d0be3f84ebf5ff2f7cdf450
Jiongyi Tan <jiongyi@localhost.localdomain>
2015-08-17 23:43:17 -0700

    rewrote replication work

--- MATLAB/cell-shape/combineexperiments.m
+++ MATLAB/cell-shape/combineexperiments.m
@@ -1,18 +1,40 @@
-function combineexperiments(propertyStr)
-	[fileNameStr, folderNameStr] = uigetfile('*.mat', ...
-		'Select .mat files storing results', 'MultiSelect', 'on');
-	if ~iscell(fileNameStr)
-		fileNameStr = {fileNameStr};
-	end
+function Experiment = combineexperiments()
+[fileNameStr, folderNameStr] = uigetfile('*.mat', ...
+    'Select .mat files storing results', 'MultiSelect', 'on');
+if ~iscell(fileNameStr)
+    fileNameStr = {fileNameStr};
+end
 
-	noFiles = numel(fileNameStr);
-	normBinCountCell = cell(1, noFiles);
-	prctStrainCell = cell(1, noFiles);
-	orientationAbsDegCell = cell(1, noFiles);
-	semPrctStrainCell = cell(1, noFiles);
-	for i = 1 : noFiles
-		load([folderNameStr, fileNameStr{i}], 'CellStatsCell');
-		[normBinCountCell{i}, prctStrainCell{i}] = ...
-			plotstrain(CellStatsCell);
-	end
+noFiles = numel(fileNameStr);
+Experiment(noFiles).orientationCell = [];
+Experiment(noFiles).meanPrctStrainRow = [];
+Experiment(noFiles).semPrctStrainRow = [];
+Experiment(noFiles).noCellsRow = [];
+Experiment(noFiles).nameStr = [];
+for i = 1 : noFiles
+    load([folderNameStr, fileNameStr{i}], 'CellStatsCell');
+    Experiment(i).nameStr = fileNameStr{i};
+    Experiment(i).orientationCell = cellfun(@(x) ...
+        abs([x(:).Orientation]), CellStatsCell, ...
+        'UniformOutput', false);
+    Experiment(i).noCellsRow = cellfun(@numel, CellStatsCell);
+    tmpMajorAxisLengthCell = cellfun(@(x) ...
+        [x(:).MajorAxisLength], CellStatsCell, ...
+        'UniformOutput', false);
+    tmpHorizProjCell = cellfun(@(x, y) x .* cos(y / 180 * pi), ...
+        tmpMajorAxisLengthCell, Experiment(i).orientationCell, ...
+        'UniformOutput', false);
+    tmpMeanHorizProjRow = cellfun(@mean, tmpHorizProjCell);
+    tmpSemHorizProjRow = cellfun(@std, tmpHorizProjCell) ./ ...
+        sqrt(Experiment(i).noCellsRow);
+    tmpNormSemHorizProjRow = tmpSemHorizProjRow ./ ...
+        tmpMeanHorizProjRow;
+    tmpNormMeanHorizProjRow = tmpMeanHorizProjRow / ...
+        tmpMeanHorizProjRow(1) - 1;
+    Experiment(i).meanPrctStrainRow = tmpNormMeanHorizProjRow;
+    Experiment(i).semPrctStrainRow = tmpNormSemHorizProjRow;
+end
+end
+
+function binbystrain(meanPrctStrainRow, semPrctStrainRow)
 end
