23019ab92a8fe6d990c7c7bc29c85867be9955de
Jiongyi Tan <jiongyi@localhost.localdomain>
2015-08-17 21:30:17 -0700

    Began work on combining results

---
+++ MATLAB/cell-shape/combineexperiments.m
@@ -0,0 +1,16 @@
+function combineexperiments(propertyStr)
+	[fileNameStr, folderNameStr] = uigetfile('*.mat', ...
+		'Select .mat files storing results', 'MultiSelect', 'on');
+	if ~iscell(fileNameStr)
+		fileNameStr = {fileNameStr};
+	end
+
+	noFiles = numel(fileNameStr);
+	normBinCountCell = cell(1, noFiles);
+	prctStrainCell = cell(1, noFiles);
+	for i = 1 : noFiles
+		load([folderNameStr, fileNameStr{i}], 'CellStatsCell');
+		[normBinCountCell{i}, prctStrainCell{i}] = ...
+			plotstrain(CellStatsCell);
+	end
+end
--- MATLAB/cell-shape/plotstrain.m
+++ MATLAB/cell-shape/plotstrain.m
@@ -1,15 +1,19 @@
-function prctStrainRow = plotstrain(CellStatsCell)
+function [prctStrainRow, semHorizProjRow] = plotstrain(CellStatsCell)
 % Initialize variables.
 noFrames = numel(CellStatsCell);
 meanHorizProjRow = zeros(1, noFrames);
+semHorizProjRow = zeros(1, noFrmaes);
 % Loop over each frame.
 for iFrame = 1 : noFrames
     tmpMajorAxisLengthRow = [CellStatsCell{iFrame}(:).MajorAxisLength];
     tmpOrientatonRadRow = [CellStatsCell{iFrame}(:).Orientation] / 180 * pi;
-    meanHorizProjRow(iFrame) = mean(tmpMajorAxisLengthRow .* ...
-        cos(tmpOrientatonRadRow));
+    tmpHorizProjRow = tmpMajorAxisLengthRow .* cos(tmpOrientationRadRow);
+    tmpNoPoints = numel(tmpHorizProjRow);
+    meanHorizProjRow(iFrame) = mean(tmpHorizProjRow);
+    semHorizProjRow(iFrame) = std(tmpHorizProjRow) / sqrt(tmpNoPoints);
 end
 % Normalize strain.
+semHorizProjRow = semHorizProjRow ./ meanHorizProjRow * 100;
 meanHorizProjRow = meanHorizProjRow / meanHorizProjRow(1);
 prctStrainRow = (meanHorizProjRow - 1) * 100;
 
