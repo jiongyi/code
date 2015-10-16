22977eb7a20eda843f6239508be5864b83b0c18e
Jiongyi Tan <joogle.t@gmail.com>
2015-10-15 18:32:43 -0700

    Got something unstable working!

--- MATLAB/actin-asters/dynamicequations.m
+++ MATLAB/actin-asters/dynamicequations.m
@@ -1,17 +1,17 @@
 function dynamicequations()
 % Initialize constants.
-meanConcentration = 100;
+meanConcentration = 200;
 noPointsPerSide = 100;
-timeInterval = 0.05;
+timeInterval = 0.05 * 3e-4;
 spaceInterval = 0.2;
 speed = 1;
 diffusionCoeff = 5;
-k = 1;
+k = 5;
 xi = 1;
 gamma = 100;
-activeTemp = 40;
-noSteps = 2;
-lambda = 1;
+activeTemp = 20;
+noSteps = 50000;
+lambda = xi * speed * meanConcentration;
 
 % Initialize concentration field matrix.
 cMat = poissrnd(meanConcentration, noPointsPerSide);
@@ -35,8 +35,6 @@ cFiltObj = zeros(3);
 cFiltObj([2, 4, 6, 8]) = 1;
 horFiltObj = [-1, 0, 1];
 verFiltObj = [-1; 0; 1];
-horDiff2FiltObj = [1, -2, 1];
-verDiff2FiltObj = [1; -2; 1];
 
 % Set up Crank-Nicolson stencil for diffusion.
 alpha = diffusionCoeff * timeInterval / (spaceInterval)^2;
@@ -59,8 +57,16 @@ end
 rhsMat(1, 1) = 1 - 0.5 * alpha;
 rhsMat(noPointsPerSide, noPointsPerSide) = 1 - 0.5 * alpha;
 
+% Set up alignment matrix stencil.
+kappa = k * timeInterval / (spaceInterval)^2;
+k1Col = -kappa * ones(noPointsPerSide - 2, 1);
+k2Col = (1 + 2 * kappa) * ones(noPointsPerSide - 2, 1);
+k3Col = -kappa * ones(noPointsPerSide -2, 1);
+
 % Iterate.
 diffusionMat = zeros(noPointsPerSide);
+uKMat = zeros(noPointsPerSide);
+vKMat = zeros(noPointsPerSide);
 for i = 1 : noSteps
     % Update relative sliding.
     uHorDiffMat = imfilter(uMat, horFiltObj, 'replicate') / ...
@@ -77,12 +83,12 @@ for i = 1 : noSteps
         lambda * vMat .* vVerDiffMat;
     
     % Update relative alignment.
-    uKMat = k * imfilter(uMat, horDiff2FiltObj, 'replicate') / ...
-        (spaceInterval^2);
-    uKMat(:, [1, end]) = 0;
-    vKMat = k * imfilter(vMat, verDiff2FiltObj, 'replicate') / ...
-        (spaceInterval^2);
-    vKMat([1, end], :) = 0;
+    for m = 1 : noPointsPerSide
+        uKMat(m, 2 : end - 1) = tridag(k1Col, k2Col, k3Col, ...
+            uMat(m, 2 : end - 1)')';
+        vKMat(2 : end - 1, m) = tridag(k1Col, k2Col, k3Col, ...
+            vMat(2 : end - 1, m));
+    end
     
     % Update contractility.
     uXiMat = xi * imfilter(cMat, horFiltObj, 'replicate');
@@ -90,10 +96,17 @@ for i = 1 : noSteps
     vXiMat = xi * imfilter(cMat, verFiltObj, 'replicate');
     vXiMat([1, end], :) = 0;
     
+    % Generate active force field.
+    uForceMat = timeInterval * activeTemp ./ cMat .* randn(noPointsPerSide);
+    uForceMat(:, [1, end]) = 0;
+    vForceMat = timeInterval * activeTemp ./ cMat .* randn(noPointsPerSide);
+    vForceMat([1, end], :) = 0;
+
     % Update advection.
     advectionMat = 0.25 * imfilter(cMat, cFiltObj, 'replicate') - ...
         beta * (imfilter(speed * uMat .* cMat, horFiltObj, 'replicate') + ...
         imfilter(speed * vMat .* cMat, verFiltObj, 'replicate'));
+    
     % Update diffusion.
     for j = 1 : noPointsPerSide
         % Rows.
@@ -108,10 +121,16 @@ for i = 1 : noSteps
     % Update concentration matrix.
     cMat = diffusionMat;
     
-    % Update orientation field.
-    uMat = uMat + timeInterval * uLambdaMat + uKMat + timeInterval * uXiMat;
-    vMat = vMat + timeInterval * vLambdaMat + vKMat + timeInterval * vXiMat;
+    % Update spontaneous polarization.
+    magMat = uMat.^2 + vMat.^2;
+    uGammaMat = timeInterval * gamma * (1 - magMat) .* uMat;
+    vGammaMat = timeInterval * gamma * (1 - magMat) .* vMat;
     
+    % Update orientation field.
+    uMat = uMat + timeInterval * uLambdaMat + uKMat + ...
+        uGammaMat + timeInterval * uXiMat + uForceMat;
+    vMat = vMat + timeInterval * vLambdaMat + vKMat + ...
+        vGammaMat + timeInterval * vXiMat + vForceMat;
     disp(num2str(sum(cMat(:))));
 end
 
@@ -119,5 +138,8 @@ figure('color', 'white');
 imshow(cMat, []); colormap jet; colorbar; axis equal off;
 
 figure('color', 'white');
-quiver(uMat, vMat, 0, 'LineWidth', 2); axis equal off;
+imshow(divergence(uMat, vMat) * spaceInterval, []); colormap jet; colorbar; axis equal off;
+hold on;
+quiver(uMat, vMat, 'LineWidth', 2, 'color', 'black');
+hold off;
 
