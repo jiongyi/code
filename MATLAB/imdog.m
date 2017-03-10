0a9f05bf846eaac50ce29c9b38f3dcfa829de29f
Jiongyi <jiongyi.tan@ucsf.edu>
2017-03-10 14:48:02 -0800

    Rewrote dog filter

--- MATLAB/imdog.m
+++ MATLAB/imdog.m
@@ -3,13 +3,9 @@ function dogIm = imdog(rawIm, pxDiameter)
 % Make gaussian filter objects.
 sigma1 = 1 / (1 +  sqrt(2)) * pxDiameter;
 sigma2 = sqrt(2) * sigma1;
-gauss1Filt = fspecial('gaussian', 6 * round(sigma1) + 1, sigma1);
-gauss2Filt = fspecial('gaussian', 6 * round(sigma2) + 1, sigma2);
 
-grayIm = im2double(rawIm);
-
-gauss1Im = imfilter(grayIm, gauss1Filt, 'replicate');
-gauss2Im = imfilter(grayIm, gauss2Filt, 'replicate');
+gauss1Im = imgaussfilt(rawIm, sigma1);
+gauss2Im = imgaussfilt(rawIm, sigma2);
 
 dogIm = gauss1Im - gauss2Im;
 dogIm = dogIm - min(dogIm(:));
