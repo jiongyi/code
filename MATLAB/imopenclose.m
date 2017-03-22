3b1f8b145bf6dbddb6a75f42f54ead7f83d36728
Jiongyi <jiongyi.tan@ucsf.edu>
2017-03-22 12:11:06 -0700

    Changed openclose to use disk strel

--- MATLAB/imopenclose.m
+++ MATLAB/imopenclose.m
@@ -1,9 +1,6 @@
 function openClosedIm = imopenclose(rawIm, radius)
 % Open-closes rawIm using a square structuring element of width objWidth.
-% Convert to double if rawIm is still uint*.
-if strcmp(class(rawIm), 'double')
-    rawIm = im2double(rawIm);
-end
+
 % Open by reconstruction.
 structEl = strel('disk', radius);
 erodedIm = imerode(rawIm, structEl);
