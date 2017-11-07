42f4f9edfc8c0627314bbc357cd1383648e539a5
Jiongyi <jiongyi.tan@ucsf.edu>
2017-11-06 16:52:52 -0800

    Added branched network simulation work

--- MATLAB/hessianfilter.m
+++ MATLAB/hessianfilter.m
@@ -1,4 +1,4 @@
-function hessianIm = hessianfilter(rawIm)
+function hessianIm = hessianfilter(rawIm, sigma)
 % Check if image is class double.
 if ~strcmp(class(rawIm), 'double')
     rawIm = im2double(rawIm);
@@ -8,6 +8,15 @@ end
 [dxMat, dyMat] = gradient(rawIm);
 [dxxMat, dxyMat] = gradient(dxMat);
 [~, dyyMat] = gradient(dyMat);
+
+% Apply gaussian filter.
+gaussFilt = fspecial('gaussian', 6 * sigma + 1, sigma);
+dxMat = imfilter(dxMat, gaussFilt, 'symmetric');
+dyMat = imfilter(dyMat, gaussFilt, 'symmetric');
+dxxMat = imfilter(dxxMat, gaussFilt, 'symmetric');
+dxyMat = imfilter(dxyMat, gaussFilt, 'symmetric');
+dyyMat = imfilter(dyyMat, gaussFilt, 'symmetric');
+
 
 bMat = -dxxMat - dyyMat;
 cMat = -dxyMat.^2 + dxxMat .* dyyMat;
